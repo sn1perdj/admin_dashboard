84213b225d8ca0506e785a982382b2c5673dffb0
Amit Singh <dj.deadshow@gmail.com>
2023-02-16 12:28:39 +0530

    first version files uploaded

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// Fullscreen
+const fullscreenButton = document.querySelector('#fullscreen-btn');
+  fullscreenButton.addEventListener('click', () => {
+    if (document.fullscreenElement) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    } else {
+      const element = document.documentElement;
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    }
+  });
